5ba781a98438ceede52da478fdbf61ea0427e675
Daniele Deltodesco <deaddread666@gmail.com>
2019-10-20 00:03:17 +0700

    finished setup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -32,6 +32,26 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
+
+# Disable request forgery protection in test environment.
+ config.action_controller.allow_forgery_protection = false
+ config.action_mailer.perform_caching = false
+# Tell Action Mailer not to deliver emails to the real world.
+ # The :test delivery method accumulates sent emails in the
+ # ActionMailer::Base.deliveries array.
+ config.action_mailer.delivery_method = :test
+ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ config.action_mailer.default_options = { from: 'cryptobalancebali@gmail.com' }
+ config.action_mailer.smtp_settings = {
+ address: 'smtp.gmail.com',
+ port: 25,
+ authentication: 'plain',
+ enable_starttls_auto: true,
+ user_name: ENV['GMAIL_USERNAME'],
+ password: ENV['GMAIL_PASSWORD'],
+ domain: ENV['DOMAIN_NAME'],
+ openssl_verify_mode: 'none',
+ }
 
   config.action_mailer.perform_caching = false
 
